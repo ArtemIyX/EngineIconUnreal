a2b6322d36f86ed0c6e62336c5cd2c7019e0ab32
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-05-01 11:51:35 +0300

    sources

---
+++ Source/EngineIconSetter/Private/EngineIconSetter.cpp
@@ -0,0 +1,53 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EngineIconSetter.h"
+
+#include "Interfaces/IPluginManager.h"
+#include "Styling/AppStyle.h"
+#include "Styling/SlateStyle.h"
+
+#define LOCTEXT_NAMESPACE "FEngineIconSetterModule"
+
+DEFINE_LOG_CATEGORY(LogEngineIconSetter);
+
+void FEngineIconSetterModule::StartupModule()
+{
+	if (FSlateStyleSet* styleSet = const_cast<FSlateStyleSet*>(static_cast<const FSlateStyleSet*>(&FAppStyle::Get())))
+	{
+		if (const TSharedPtr<IPlugin> plugin = IPluginManager::Get().FindPlugin(TEXT("EngineIconSetter")))
+		{
+			const FString iconPath = plugin->GetBaseDir() / TEXT("Resources/Icon.svg");
+			previousAppIcon = styleSet->GetBrush("AppIcon");
+			styleSet->Set("AppIcon", new FSlateVectorImageBrush(iconPath, FVector2D(48.0f, 48.0f)));
+			bAppIconOverridden = true;
+		}
+		else
+		{
+			UE_LOG(LogEngineIconSetter, Error, TEXT("EngineIconSetter plugin was not found"));
+		}
+	}
+	else
+	{
+		UE_LOG(LogEngineIconSetter, Error, TEXT("StyleSet is null"));
+	}
+}
+
+void FEngineIconSetterModule::ShutdownModule()
+{
+	if (!bAppIconOverridden || !previousAppIcon)
+	{
+		return;
+	}
+
+	if (FSlateStyleSet* styleSet = const_cast<FSlateStyleSet*>(static_cast<const FSlateStyleSet*>(&FAppStyle::Get())))
+	{
+		styleSet->Set("AppIcon", const_cast<FSlateBrush*>(previousAppIcon));
+	}
+
+	previousAppIcon = nullptr;
+	bAppIconOverridden = false;
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FEngineIconSetterModule, EngineIconSetter)
---
+++ Source/EngineIconSetter/Public/EngineIconSetter.h
@@ -0,0 +1,23 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+#include "Styling/SlateBrush.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogEngineIconSetter, Log, All);
+
+class FEngineIconSetterModule : public IModuleInterface
+{
+public:
+#pragma region ModuleOverrides
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+#pragma endregion
+
+#pragma region Properties
+private:
+	const FSlateBrush* previousAppIcon = nullptr;
+	bool bAppIconOverridden = false;
+#pragma endregion
+};
